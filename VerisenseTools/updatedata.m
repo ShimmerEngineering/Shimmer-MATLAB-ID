bb83f05ac1a93a3afa26d60d9cc6a71120d66c1e
Tan Wei Wen <weiwentan@shimmersensing.com>
2022-11-24 09:46:09 +0800

    add update verisense data function

---
+++ VerisenseTools/updatedata.m
@@ -0,0 +1,25 @@
+function void = updatedata( filename, noise, offset, resolution, alignment)
+
+data = importdata(filename);
+if noise~=0
+    xmin = -noise;
+    xmax = noise;
+    noise_matrix = xmin+rand(size(data.data,1),3)*(xmax-xmin);
+    data.data = data.data + noise_matrix;
+end
+
+if offset~=0
+    offset_matrix = zeros(size(data.data,1),3) + offset;
+    data.data = data.data + offset_matrix;
+end
+
+if resolution~=0
+    temp = ceil(1 / resolution);
+    data.data = ceil(data.data * temp) / temp;
+end
+
+data.data = data.data * alignment;
+
+csvwrite('combined_file.csv', data.data);
+
+end
