0b114ad5fc76cae917d42d816bbded568b4a7b72
Harith Jamadi <114632577+harithjamadi@users.noreply.github.com>
2024-09-03 13:31:45 +0800

    some changes on the sensor data buffer

---
+++ MatlabTest/src/ShimmerJavaClass.java
@@ -0,0 +1,196 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.shimmerresearch.bluetooth.ShimmerBluetooth.BT_STATE;
+import com.shimmerresearch.driver.BasicProcessWithCallBack;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.ObjectCluster;
+import com.shimmerresearch.driver.ShimmerMsg;
+import com.shimmerresearch.driver.Configuration.COMMUNICATION_TYPE;
+import com.shimmerresearch.driverUtilities.ChannelDetails;
+import com.shimmerresearch.exceptions.ShimmerException;
+import com.shimmerresearch.pcDriver.ShimmerPC;
+
+public class ShimmerJavaClass {
+    private ShimmerPC shimmer;
+    private String comPort = "COM8";
+    private SensorDataReceived sdr = new SensorDataReceived();
+    private String[] channelNames;
+    private final Queue<float[]> dataQueue = new ConcurrentLinkedQueue<>();
+
+    public static void main(String[] args) {
+        ShimmerJavaClass example = new ShimmerJavaClass();
+        example.createAndShowGUI();
+    }
+
+    public void createAndShowGUI() {
+        JFrame frame = new JFrame("Shimmer Device Controller");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 200);
+
+        JPanel panel = new JPanel();
+
+        JButton connectButton = new JButton("Connect");
+        JButton disconnectButton = new JButton("Disconnect");
+        JButton startButton = new JButton("Start Streaming");
+        JButton stopButton = new JButton("Stop Streaming");
+        
+        connectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                connectDevice(comPort);
+            }
+        });
+
+        disconnectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                disconnectDevice();
+            }
+        });
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startStreaming();
+            }
+        });
+
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                stopStreaming();
+            }
+        });
+
+        panel.add(connectButton);
+        panel.add(disconnectButton);
+        panel.add(startButton);
+        panel.add(stopButton);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+
+    public void connectDevice(String comPort) {
+        shimmer = new ShimmerPC("Test");
+        shimmer.connect(comPort, "");
+        sdr.setWaitForData(shimmer);
+    }
+
+    public void disconnectDevice() {
+        try {
+            shimmer.disconnect();
+        } catch (ShimmerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startStreaming() {
+        try {
+            shimmer.startStreaming();
+        } catch (ShimmerException e) {
+            e.printStackTrace();
+        }
+        
+//        try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//        
+//        while(true) {
+//            try {
+//    			Thread.sleep(8);
+//    	        readData();
+//    		} catch (InterruptedException e) {
+//    			// TODO Auto-generated catch block
+//    			e.printStackTrace();
+//    		}
+//        }
+    }
+
+    public void stopStreaming() {
+        shimmer.stopStreaming();
+    }
+
+    public String[] retrieveSensorChannels() {
+        LinkedHashMap<String, ChannelDetails> mapOfAllChannels = shimmer.getMapOfAllChannelsForStoringToDB(COMMUNICATION_TYPE.BLUETOOTH, null, false, false);
+        List<ChannelDetails> listOfChannelDetails = new ArrayList<>(mapOfAllChannels.values());
+
+        List<String> channelNamesList = new ArrayList<>();
+        for (ChannelDetails channel : listOfChannelDetails) {
+            channelNamesList.add(channel.mObjectClusterName);
+        }
+
+        return channelNamesList.toArray(new String[0]);
+    }
+
+    public float receiveData(String channel) { //Receive data for specific Object Cluster
+        float[] data = dataQueue.poll();
+		for (int i = 0; i < channelNames.length; i++) {
+		    if (channelNames[i].equals(channel)) {
+		        return data[i];
+		    }
+		}
+        return Float.NaN;
+    }
+    
+    public float[] receiveData() { //Receive data for all Object Cluster
+        float[] data = dataQueue.poll();
+        return data;
+    }
+
+    private void sendData(float[] data) {
+        dataQueue.add(data);
+    }
+    
+//    public void readData() { //Test receive data for all Object Cluster
+//    	float[] data = receiveData();
+//    	System.out.println("Timestamp : " + data[4] + "\nAccel_LN_X : " + data[0]);
+//    }
+
+    public class SensorDataReceived extends BasicProcessWithCallBack {
+
+        @Override
+        protected void processMsgFromCallback(ShimmerMsg shimmerMSG) {
+            int ind = shimmerMSG.mIdentifier;
+            Object object = shimmerMSG.mB;
+
+            if (ind == ShimmerPC.MSG_IDENTIFIER_STATE_CHANGE) {
+                CallbackObject callbackObject = (CallbackObject) object;
+
+                if (callbackObject.mState == BT_STATE.CONNECTING) {
+                	
+                } else if (callbackObject.mState == BT_STATE.CONNECTED) {
+                	channelNames = retrieveSensorChannels();
+                	for(String channel : channelNames) {
+                		System.out.println(channel);
+                	}
+                } else if (callbackObject.mState == BT_STATE.DISCONNECTED || callbackObject.mState == BT_STATE.CONNECTION_LOST) {
+
+                }
+            } else if (ind == ShimmerPC.MSG_IDENTIFIER_NOTIFICATION_MESSAGE) {
+
+            } else if (ind == ShimmerPC.MSG_IDENTIFIER_DATA_PACKET) {
+                ObjectCluster objc = (ObjectCluster) shimmerMSG.mB;
+
+                float[] data = new float[channelNames.length];
+                for (int i = 0; i < channelNames.length; i++) {
+                    data[i] = (float) objc.getFormatClusterValue(channelNames[i], "CAL");
+                }
+
+                sendData(data);
+            }
+        }
+    }
+}
--- connectstreamandplotexample.m
+++ connectstreamandplotexample.m
@@ -1,4 +1,4 @@
-clear java
+clear java;
 javaaddpath('libs/ShimmerJavaClass.jar');
 javaaddpath('libs/jssc-2.9.6.jar');
 javaaddpath('libs/commons-math-2.2.jar');
@@ -9,62 +9,63 @@ obj = ShimmerJavaClass();
 
 obj.connectDevice('COM8');
 
-%% Display list of Object Cluster
+%%
+% obj.configureDevice();
+disp('File created.');
+sensorName = {"Accel_LN_X", "Timestamp"};
+csvFileName = 'sensor_data.csv';
+csvFileID = fopen(csvFileName, 'w');
+fprintf(csvFileID, 'Index,Timestamp,%s\n', sensorName{1});
 
-obj.configureDevice();
-
-%% 
-
-obj.startStreaming();
+%%
 disp('Streaming started.');
-
-%%
-while true
-    data = obj.receiveData("Accel_LN_X");
-    disp(data);
-    pause(0.01);
-end
-
-%%
+obj.startStreaming();
 
 figure;
 hPlot = plot(nan, nan);
 xlabel('Index');
 ylabel('Data');
-title('Real-Time Data Plot');
+title(sensorName{1} + " Plot");
 
 indexVec = [];
-
 dataVec = [];
 
-while true
-    data = obj.receiveData("Accel_LN_X");
-    
-    indexVec = [indexVec, length(indexVec) + 1];
-    dataVec = [dataVec, data];
-    
-    set(hPlot, 'XData', indexVec, 'YData', dataVec);
-    
-    if length(indexVec) > 1
-        xlim([1, length(indexVec)]);
+try
+    while true
+        data = obj.receiveData();
+
+        if ~isempty(data) && numel(data) >= 5
+            disp(['Data: ', num2str(data(1)), ', Timestamp: ', num2str(data(5))]);
+
+            indexVec = [indexVec, length(indexVec) + 1];
+            dataVec = [dataVec, data(1)];
+
+            set(hPlot, 'XData', indexVec, 'YData', dataVec);
+
+            if length(indexVec) > 1
+                xlim([1, length(indexVec)]);
+            end
+
+            if length(dataVec) > 1
+                ylim([min(dataVec), max(dataVec)]);
+            end
+
+            fprintf(csvFileID, '%d,%.6f,%.6f\n', length(indexVec), data(5), data(1));
+        else
+            disp('Received invalid or incomplete data.');
+        end
+
+        drawnow;
     end
-    
-    if length(dataVec) > 1
-        ylim([min(dataVec), max(dataVec)]);
-    end
-    
-    drawnow;
-    pause(0.01);
+catch e
+    disp('Streaming interrupted.');
+    disp(e.message);
 end
 
 %%
-
-obj.configureDevice();
-
-%%
+fclose(csvFileID);
 
 obj.stopStreaming();
-
-%%
-
 obj.disconnectDevice();
+
+disp('Streaming stopped. Data saved to CSV file.');
