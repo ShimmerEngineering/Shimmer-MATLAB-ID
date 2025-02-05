5da47a80994d46abf8cd5cde4756b989f669cce9
Harith Jamadi <114632577+harithjamadi@users.noreply.github.com>
2025-02-05 13:17:46 +0800

    update ppgtohr example function

--- test.m
+++ test.m
@@ -1,5 +1,14 @@
+%%
 comPort = 'COM3';
 captureDuration = 20;
 fileName = 'DefaultTrial_Session1_test_Calibrated_SD';
 
 newplotandwriteexample(comPort, captureDuration, fileName);
+
+%%
+comPort = 'COM3';
+PPGChannelNum = 13;
+captureDuration = 20;
+fileName = 'DefaultTrial_Session1_test_Calibrated_PC_PPG';
+
+newppgtoheartrateexample(comPort, PPGChannelNum, captureDuration, fileName);
