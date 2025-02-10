c712cdcd116c627083da111419093b28500bd9b7
Harith Jamadi <114632577+harithjamadi@users.noreply.github.com>
2025-02-10 09:14:31 +0800

    update enablingsensor method for matlab

--- ShimmerDeviceHandler.m
+++ ShimmerDeviceHandler.m
@@ -31,6 +31,7 @@ classdef ShimmerDeviceHandler
                 if ~isempty(this.shimmer)
                     disp(['Device connected... Elapsed time: ', num2str(toc(startTime)), ' seconds']);
                     success = true;
+                    pause(10);
                     return;
                 end
             end
@@ -45,5 +46,39 @@ classdef ShimmerDeviceHandler
             this.shimmer.startStreaming();
             success = true;
         end
+        
+        function enabledsensors(this, samplingRate, sensorsArray)
+            
+            %   Valid values for SENSORNAME are:
+            %       'LowNoiseAccel', 'WideRangeAccel', 'AlternativeAccel', 'Gyro',
+            %       'Mag', 'AlternativeMag', 'ECG', 'EMG', 'EXG', 'GSR',
+            %       'EXT A7', 'EXT A9', 'EXT A6', 'EXT A11', 'EXT A15', 'EXT A2',
+            %       'Bridge Amplifier', 'BattVolt', 'INT A1', 'INT A7', 'INT A12',
+            %       'INT A10', 'INT A13', 'INT A15', 'INT A14', 'INT A16', 'Pressure'
+            
+            this.shimmer = this.obj.mBluetoothManager.getShimmerDeviceBtConnected(this.comPort);
+
+            shimmerClone = this.shimmer.deepClone();
+            shimmerClone.setSamplingRateShimmer(samplingRate);
+            shimmerClone.disableAllSensors();
+            shimmerClone.setEnabledAndDerivedSensorsAndUpdateMaps(0, 0);
+
+            for iSensor = 1:length(sensorsArray)
+                
+                sensorName = sensorsArray{iSensor};
+                enabledSensor = this.obj.getSensorId(sensorName);
+                shimmerClone.setSensorEnabledState(enabledSensor, true);
+            end
+
+            commType = javaMethod('valueOf', 'com.shimmerresearch.driver.Configuration$COMMUNICATION_TYPE', 'BLUETOOTH');
+            com.shimmerresearch.driverUtilities.AssembleShimmerConfig.generateSingleShimmerConfig(shimmerClone, commType);
+            this.shimmer.configureFromClone(shimmerClone);
+
+            btState = javaMethod('valueOf', 'com.shimmerresearch.bluetooth.ShimmerBluetooth$BT_STATE', 'CONFIGURING');
+            this.shimmer.operationStart(btState);
+
+            pause(20);
+        end
+
     end
 end
